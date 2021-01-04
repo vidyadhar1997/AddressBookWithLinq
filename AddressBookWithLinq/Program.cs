c84fad7f11810e7b5ac21198ac3368dc476c1c4e
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2021-01-04 18:54:25 +0530

    [vidyadhar hudge]Add:Welcome message

---
+++ AddressBookWithLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookWithLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the address book using linq");
+        }
+    }
+}
